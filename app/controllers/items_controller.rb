8ebce09839c9c3a3f68042c20b42689f25a87e56
Mario Zugaj <mario@kasaz.com>
2019-07-21 12:08:31 +0200

    Add ItemsChart

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+    @chart_data = [
+      number_of_keys(@items.first.input_data).size,
+      number_of_keys(@items.first.output_data).size
+    ]
   end
 
   def create
@@ -11,6 +15,13 @@ class ItemsController < ApplicationController
       render json: { item: outcome.result }
     else
       render json: outcome.errors.symbolic, status: 422
+    end
+  end
+
+  def number_of_keys(hash)
+    hash.each_with_object([]) do |(key, value), keys|
+      keys << key
+      keys.concat(number_of_keys(value)) if value.is_a? Hash
     end
   end
 end
